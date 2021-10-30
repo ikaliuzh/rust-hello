bb7c03e3bd086cba2cce11384670a01dd43d28d1
ikaliuzh <ikaliuzh@gmail.com>
2021-10-30 20:26:56 +0300

    server-client

---
+++ hello-web/src/server.rs
@@ -0,0 +1,42 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream){
+    let mut data = [0 as u8; 50];
+    while match stream.read(&mut data){
+        Ok(size) => {
+            println!("Message received: {:?}", data);
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+
+fn main() {
+    let port = 3333;
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).unwrap();
+    println!("Server listening on port {}", port);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            },
+            Err(e) => {
+                println!("Error: {}", e);
+            },
+        }
+    }
+
+    drop(listener);
+}
